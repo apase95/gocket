95a8244c973f2a86b9bcca917846698eeccd1a2d
apase95 <noobyhandsome@gmail.com>
2026-06-20 17:54:41 +0700

    feat: [TSK-002][TSK-003] Configuration websocket upgrader and define struct client

---
+++ server/hub.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Hub struct {
+
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 	1024,
+	WriteBufferSize: 	1024,
+	CheckOrigin: 		func(r *http.Request) bool {
+		return true
+	},
+}
+
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
