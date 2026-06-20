65e75efd1ba4870a0061c844d056ff4eaf2df72a
apase95 <noobyhandsome@gmail.com>
2026-06-20 23:04:51 +0700

    feat: [TSK-003][TSK-004][TSK-005] implement Hub struct, Run func, Read/Write Pump for Client

--- server/client.go
+++ server/client.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -8,21 +9,70 @@ import (
 )
 
 type Client struct {
-	ID 			string
-	Conn 		*websocket.Conn
-	Send 		chan []byte
-	hub 		*Hub
-	rooms		map[string]bool
+	ID          string
+	Conn        *websocket.Conn
+	Send        chan []byte
+	hub         *Hub
+	rooms       map[string]bool
 	connectedAt time.Time
+	once        sync.Once
 }
 
 func NewClient(conn *websocket.Conn, hub *Hub) *Client {
 	return &Client{
-		ID: 			uuid.New().String(),
-		Conn: 			conn,
-		Send: 			make(chan []byte, 256),
-		hub: 			hub, 
-		rooms: 			make(map[string]bool),
-		connectedAt: 	time.Now(),
+		ID:          uuid.New().String(),
+		Conn:        conn,
+		Send:        make(chan []byte, 256),
+		hub:         hub,
+		rooms:       make(map[string]bool),
+		connectedAt: time.Now(),
+	}
+}
+
+func (c *Client) Close() {
+	c.once.Do(func() {
+		close(c.Send)
+		if c.Conn != nil {
+			c.Conn.Close()
+		}
+	})
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		c.hub.dispatch(c, message)
+	}
+}
+
+func (c *Client) writePump() {
+	defer func() {
+		if c.Conn != nil {
+			c.Conn.Close()
+		}
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.Send:
+			if !ok {
+				if c.Conn != nil {
+					_ = c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				}
+				return
+			}
+			err := c.Conn.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				return
+			}
+		}
 	}
 }
--- server/hub.go
+++ server/hub.go
@@ -2,18 +2,53 @@ package server
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 type Hub struct {
+	clients    map[string]*Client
+	register   chan *Client
+	unregister chan *Client
+	mu         sync.RWMutex
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[string]*Client),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.mu.Lock()
+			h.clients[client.ID] = client
+			h.mu.Unlock()
+
+		case client := <-h.unregister:
+			h.mu.Lock()
+			if _, ok := h.clients[client.ID]; ok {
+				delete(h.clients, client.ID)
+				client.Close()
+			}
+			h.mu.Unlock()
+		}
+	}
+}
+
+func (h *Hub) dispatch(client *Client, payload []byte) {
 
 }
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize: 	1024,
-	WriteBufferSize: 	1024,
-	CheckOrigin: 		func(r *http.Request) bool {
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
